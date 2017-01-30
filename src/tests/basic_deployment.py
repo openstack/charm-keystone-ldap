33f4796ec6f4405725b82dbaa353e6ee39005018
James Page <james.page@ubuntu.com>
2017-01-30 12:33:13 +0000

    Add basic amulet tests, with AMULET overrides for configuration

--- src/tests/basic_deployment.py
+++ src/tests/basic_deployment.py
@@ -1,20 +1,21 @@
-# Copyright 2016 Canonical Ltd
+#
+# Copyright 2017 Canonical Ltd
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 
 import amulet
-import json
-import subprocess
-import time
-
 
 import charmhelpers.contrib.openstack.amulet.deployment as amulet_deployment
 import charmhelpers.contrib.openstack.amulet.utils as os_amulet_utils
@@ -23,13 +24,13 @@ import charmhelpers.contrib.openstack.amulet.utils as os_amulet_utils
 u = os_amulet_utils.OpenStackAmuletUtils(os_amulet_utils.DEBUG)
 
 
-class SDNCharmDeployment(amulet_deployment.OpenStackAmuletDeployment):
+class KeystoneLDAPCharmDeployment(amulet_deployment.OpenStackAmuletDeployment):
     """Amulet tests on a basic sdn_charm deployment."""
 
     def __init__(self, series, openstack=None, source=None, stable=False):
         """Deploy the entire test environment."""
-        super(SDNCharmDeployment, self).__init__(series, openstack,
-                                                       source, stable)
+        super(KeystoneLDAPCharmDeployment, self).__init__(series, openstack,
+                                                          source, stable)
         self._add_services()
         self._add_relations()
         self._configure_services()
@@ -48,63 +49,65 @@ class SDNCharmDeployment(amulet_deployment.OpenStackAmuletDeployment):
            and the rest of the service are from lp branches that are
            compatible with the local charm (e.g. stable or next).
            """
-        this_service = {'name': 'sdn_charm'}
+        this_service = {'name': 'keystone-ldap'}
         other_services = [
-            {
-                'name': 'nova-compute',
-                'constraints': {'mem': '4G'},
-            },
-            {
-                'name': 'neutron-api',
-            },
-            {
-                'name': 'neutron-gateway',
-            },
-            {'name': 'mysql'},
-            {'name': 'rabbitmq-server'},
             {'name': 'keystone'},
-            {'name': 'nova-cloud-controller'},
-            {'name': 'glance'},
+            {'name': 'percona-cluster', 'constraints': {'mem': '3072M'}},
         ]
-        super(SDNCharmDeployment, self)._add_services(this_service,
-                                                      other_services)
+        super(KeystoneLDAPCharmDeployment, self)._add_services(this_service,
+                                                               other_services)
 
     def _add_relations(self):
         """Add all of the relations for the services."""
         relations = {
-            'nova-compute:neutron-plugin': 'sdn_charm:neutron-plugin',
-            'keystone:shared-db': 'mysql:shared-db',
-            'nova-cloud-controller:shared-db': 'mysql:shared-db',
-            'nova-cloud-controller:amqp': 'rabbitmq-server:amqp',
-            'nova-cloud-controller:image-service': 'glance:image-service',
-            'nova-cloud-controller:identity-service':
-            'keystone:identity-service',
-            'nova-compute:cloud-compute':
-            'nova-cloud-controller:cloud-compute',
-            'nova-compute:amqp': 'rabbitmq-server:amqp',
-            'nova-compute:image-service': 'glance:image-service',
-            'glance:shared-db': 'mysql:shared-db',
-            'glance:identity-service': 'keystone:identity-service',
-            'glance:amqp': 'rabbitmq-server:amqp',
-            'neutron-api:shared-db': 'mysql:shared-db',
-            'neutron-api:amqp': 'rabbitmq-server:amqp',
-            'neutron-api:neutron-api': 'nova-cloud-controller:neutron-api',
-            'neutron-api:identity-service': 'keystone:identity-service',
-            'neutron-gateway:amqp': 'rabbitmq-server:amqp',
-            'neutron-gateway:neutron-plugin-api':
-            'neutron-api:neutron-plugin-api',
-            'neutron-gateway:quantum-network-service':
-            'nova-cloud-controller:quantum-network-service',
-            'neutron-gateway:juju-info': 'sdn_charm:container',
+            'keystone:domain-backend': 'keystone-ldap:domain-backend',
+            'keystone:shared-db': 'percona-cluster:shared-db',
         }
-        super(SDNCharmDeployment, self)._add_relations(relations)
+        super(KeystoneLDAPCharmDeployment, self)._add_relations(relations)
 
     def _configure_services(self):
         """Configure all of the services."""
-        keystone_config = {'admin-password': 'openstack',
-                           'admin-token': 'ubuntutesting'}
-        configs = {'keystone': keystone_config}
-        super(SDNCharmDeployment, self)._configure_services(configs)
+        keystone_config = {
+            'admin-password': 'openstack',
+            'admin-token': 'ubuntutesting',
+            'preferred-api-version': 3,
+        }
+        keystone_ldap_config = self._get_ldap_config()
+        pxc_config = {
+            'dataset-size': '25%',
+            'max-connections': 1000,
+            'root-password': 'ChangeMe123',
+            'sst-password': 'ChangeMe123',
+        }
+        configs = {'keystone': keystone_config,
+                   'keystone-ldap': keystone_ldap_config,
+                   'percona-cluster': pxc_config}
+        super(KeystoneLDAPCharmDeployment, self)._configure_services(configs)
+
+    def _get_ldap_config(self):
+        # NOTE(jamespage): use amulet variables for CI specific config
+        keystone_ldap_config = {
+            'ldap-server': os.environ.get('AMULET_LDAP_SERVER'),
+            'ldap-user': os.environ.get('AMULET_LDAP_USER'),
+            'ldap-password': os.environ.get('AMULET_LDAP_PASSWORD'),
+            'ldap-suffix': os.environ.get('AMULET_LDAP_SUFFIX'),
+            'domain-name': 'userdomain',
+        }
+        if all(keystone_ldap_config.values()):
+            self.ldap_configured = True
+            return keystone_ldap_config
+        else:
+            # NOTE(jamespage): Use mock values to check deployment only
+            #                  as no test fixture has been supplied
+            self.ldap_configured = False
+            return {
+                'ldap-server': 'myserver',
+                'ldap-user': 'myuser',
+                'ldap-password': 'mypassword',
+                'ldap-suffix': 'mysuffix',
+                'domain-name': 'userdomain',
+            }
+
 
     def _get_token(self):
         return self.keystone.service_catalog.catalog['token']['id']
@@ -112,55 +115,14 @@ class SDNCharmDeployment(amulet_deployment.OpenStackAmuletDeployment):
     def _initialize_tests(self):
         """Perform final initialization before tests get run."""
         # Access the sentries for inspecting service units
-        self.sdn_charm_sentry = self.d.sentry['sdn_charm'][0]
-        self.mysql_sentry = self.d.sentry['mysql'][0]
+        self.keystone_ldap = self.d.sentry['keystone-ldap'][0]
+        self.mysql_sentry = self.d.sentry['percona-cluster'][0]
         self.keystone_sentry = self.d.sentry['keystone'][0]
-        self.rabbitmq_sentry = self.d.sentry['rabbitmq-server'][0]
-        self.sdn_charm_svcs = [
-            'sdn_charm-agent', 'sdn_charm-api']
-
         # Authenticate admin with keystone endpoint
         self.keystone = u.authenticate_keystone_admin(self.keystone_sentry,
                                                       user='admin',
                                                       password='openstack',
                                                       tenant='admin')
-
-    def check_and_wait(self, check_command, interval=2, max_wait=200,
-                       desc=None):
-        waited = 0
-        while not check_command() or waited > max_wait:
-            if desc:
-                u.log.debug(desc)
-            time.sleep(interval)
-            waited = waited + interval
-        if waited > max_wait:
-            raise Exception('cmd failed {}'.format(check_command))
-
-    def _run_action(self, unit_id, action, *args):
-        command = ["juju", "action", "do", "--format=json", unit_id, action]
-        command.extend(args)
-        print("Running command: %s\n" % " ".join(command))
-        output = subprocess.check_output(command)
-        output_json = output.decode(encoding="UTF-8")
-        data = json.loads(output_json)
-        action_id = data[u'Action queued with id']
-        return action_id
-
-    def _wait_on_action(self, action_id):
-        command = ["juju", "action", "fetch", "--format=json", action_id]
-        while True:
-            try:
-                output = subprocess.check_output(command)
-            except Exception as e:
-                print(e)
-                return False
-            output_json = output.decode(encoding="UTF-8")
-            data = json.loads(output_json)
-            if data[u"status"] == "completed":
-                return True
-            elif data[u"status"] == "failed":
-                return False
-            time.sleep(2)
 
     def test_100_services(self):
         """Verify the expected services are running on the corresponding
@@ -168,7 +130,7 @@ class SDNCharmDeployment(amulet_deployment.OpenStackAmuletDeployment):
         u.log.debug('Checking system services on units...')
 
         service_names = {
-            self.sdn_charm_sentry: self.sdn_charm_svcs,
+            self.keystone_ldap: [],
         }
 
         ret = u.validate_services_by_name(service_names)
